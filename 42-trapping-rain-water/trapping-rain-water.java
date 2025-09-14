69029674e1b2a83f46c73f89b1300af6d1e211fe
Mahammad Aftab Umachagi <mdaftabeditz360@gmail.com>
2025-09-14 19:05:07 +0530

    Time: 1 ms (61.23%) | Memory: 46.1 MB (74.34%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -12,9 +12,7 @@ class Solution {
      * Output: 6
      * Explanation: The above elevation map (black section) is represented by array [0,1,0,2,1,0,1,3,2,1,2,1].
      * In this case, 6 units of rain water (blue section) are being trapped.
-     *
-     * @param height An array of non-negative integers representing the elevation map.
- 
+     
      */
     public int trap(int[] height) {
         if (height == null || height.length == 0) {
