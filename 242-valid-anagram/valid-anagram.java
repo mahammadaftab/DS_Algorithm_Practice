d79c866d3713931036c17d41f9c0877fdedf9fd2
Mahammad Aftab Umachagi <mdaftabeditz360@gmail.com>
2025-09-15 22:16:08 +0530

    Time: 4 ms (76.62%) | Memory: 44.7 MB (35.00%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -4,9 +4,6 @@ class Solution {
     /**
      * Given two strings s and t, return true if t is an anagram of s, and false otherwise.
      *
-     * An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase,
-     * typically using all the original letters exactly once.
-     *
 
      */
     public boolean isAnagram(String s, String t) {
