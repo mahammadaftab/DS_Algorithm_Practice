3f0ef45e41ccef528d0192a2d79fceb76dcd6af8
Mahammad Aftab Umachagi <mdaftabeditz360@gmail.com>
2025-09-15 22:14:59 +0530

    Time: 3 ms (91.23%) | Memory: 44.7 MB (35.00%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -14,10 +14,6 @@ class Solution {
      * Example 2:
      * Input: s = "rat", t = "car"
      * Output: false
-     *
-     * Constraints:
-     * 1 <= s.length, t.length <= 5 * 104
-     * s and t consist of lowercase English letters.
 
      */
     public boolean isAnagram(String s, String t) {
