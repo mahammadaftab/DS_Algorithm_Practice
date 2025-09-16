5aa3e63d74effd9974bcb679c1e7721444f6c4d6
Mahammad Aftab Umachagi <mdaftabeditz360@gmail.com>
2025-09-16 21:49:02 +0530

    Time: 32 ms (31.71%) | Memory: 45.3 MB (66.48%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
@@ -17,10 +17,6 @@ class Solution {
      * Example 3:
      * Input: s = "aabb"
      * Output: -1
-     *
-     * Constraints:
-     * 1 <= s.length <= 105
-     * @return the index of the first non-repeating character, or -1 if it does not exist
      */
     public int firstUniqChar(String s) {
         // Use a HashMap to store the frequency of each character in the string.
