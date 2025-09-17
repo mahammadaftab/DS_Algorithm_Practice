027a3afe588488d19d5884f6457fb20782d46cd4
Mahammad Aftab Umachagi <mdaftabeditz360@gmail.com>
2025-09-17 21:22:55 +0530

    Time: 6 ms (98.82%) | Memory: 48 MB (56.14%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,9 +1,6 @@
 import java.util.*;
 
 public class Solution {
-
-    /**
-     */
     public List<List<String>> groupAnagrams(String[] strs) {
         if (strs == null || strs.length == 0) {
             return new ArrayList<>();
