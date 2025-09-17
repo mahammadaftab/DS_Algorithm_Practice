31c26f5aedf88af748223e646eed9f56b1e724c2
Mahammad Aftab Umachagi <mdaftabeditz360@gmail.com>
2025-09-17 21:22:17 +0530

    Time: 6 ms (98.82%) | Memory: 47.8 MB (83.85%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -4,9 +4,6 @@ public class Solution {
 
     /**
      * Given an array of strings strs, group the anagrams together. You can return the answer in any order.
-     * An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase,
-     * typically using all the original letters exactly once.
-     *
      */
     public List<List<String>> groupAnagrams(String[] strs) {
         if (strs == null || strs.length == 0) {
