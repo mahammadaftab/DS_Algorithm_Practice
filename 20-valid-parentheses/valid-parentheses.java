4c3b30ad3fd7c4731ebbce79ae97031be9f6d597
Mahammad Aftab Umachagi <mdaftabeditz360@gmail.com>
2025-09-18 22:06:24 +0530

    Time: 2 ms (96.81%) | Memory: 41.9 MB (58.78%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -8,10 +8,6 @@ class Solution {
      *
      * Open brackets must be closed by the same type of brackets.
      * Open brackets must be closed in the correct order.
-     * Every close bracket has a corresponding open bracket of the same type.
-     *
-     * @param s The input string containing parentheses.
-     * @return true if the string is valid, false otherwise.
      */
     public boolean isValid(String s) {
         // Use a stack to keep track of opening parentheses.
