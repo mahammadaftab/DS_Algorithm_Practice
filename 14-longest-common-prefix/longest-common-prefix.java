53abfcff29bd4fdd7a1eb6cd5fb1576f048f7bec
Mahammad Aftab Umachagi <mdaftabeditz360@gmail.com>
2025-09-20 22:09:47 +0530

    Time: 1 ms (60.59%) | Memory: 41.6 MB (62.71%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -18,9 +18,6 @@ class Solution {
      * Explanation: There is no common prefix among the input strings.
      *
      * Constraints:
-     * 1 <= strs.length <= 200
-     * 0 <= strs[i].length <= 200
-     * strs[i] consists of only lowercase English letters.
      */
     public String longestCommonPrefix(String[] strs) {
         if (strs == null || strs.length == 0) {
