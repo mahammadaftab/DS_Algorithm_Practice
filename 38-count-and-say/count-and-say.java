20a2e905f4ad6fec70f8a9275931eae5679f0a30
Mahammad Aftab Umachagi <mdaftabeditz360@gmail.com>
2025-09-22 22:17:23 +0530

    Time: 2 ms (91.93%) | Memory: 41.4 MB (64.80%) - LeetSync

--- 38-count-and-say/count-and-say.java
+++ 38-count-and-say/count-and-say.java
@@ -7,8 +7,7 @@ class Solution {
      * The count-and-say sequence is a sequence of digit strings defined by the recursive formula:
      * 
      * countAndSay(1) = "1"
-     * countAndSay(n) is the way you would "say" the digit string from countAndSay(n-1), which is then converted into a different digit string.
-        */
+          */
     public String countAndSay(int n) {
         if (n == 1) {
             return "1";
