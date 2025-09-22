a48fdd298b5dcfd1083ad2ca2f39d61fa289a7b7
Mahammad Aftab Umachagi <mdaftabeditz360@gmail.com>
2025-09-22 22:12:22 +0530

    Time: 1 ms (99.26%) | Memory: 41.3 MB (64.80%) - LeetSync

--- 38-count-and-say/count-and-say.java
+++ 38-count-and-say/count-and-say.java
@@ -23,7 +23,6 @@ class Solution {
      * Explanation:
      * countAndSay(1) = "1"
      * countAndSay(2) = say "1" = one 1 = "11"
-     * countAndSay(3) = say "11" = two 1s = "21"
      */
     public String countAndSay(int n) {
         if (n == 1) {
