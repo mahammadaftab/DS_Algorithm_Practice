1cd3c3eb60bc19418d5fefbeb254dbdad29d0fa9
Mahammad Aftab Umachagi <mdaftabeditz360@gmail.com>
2025-09-25 22:36:24 +0530

    Time: 15 ms (45.54%) | Memory: 44.7 MB (82.68%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -5,10 +5,6 @@ import java.util.List;
 public class Solution {
 
     /**
-     * You may return the answer in any order.
-     *
-     * An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase,
-     * typically using all the original letters exactly once.
      *
      * Example 1:
      * Input: s = "cbaebabacd", p = "abc"
