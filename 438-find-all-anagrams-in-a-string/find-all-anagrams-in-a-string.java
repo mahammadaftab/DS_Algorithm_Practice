0fdb064c3698bcf0de7541675af999c9411a62e3
Mahammad Aftab Umachagi <mdaftabeditz360@gmail.com>
2025-09-25 22:33:47 +0530

    Time: 15 ms (45.54%) | Memory: 44.7 MB (82.68%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -24,8 +24,6 @@ public class Solution {
      * Input: s = "abab", p = "ab"
      * Output: [0,1,2]
      * Explanation:
-     * The substring with start index = 0 is "ab", which is an anagram of "ab".
-     * The substring with start index = 1 is "ba", which is an anagram of "ab".
      */
     public List<Integer> findAnagrams(String s, String p) {
         List<Integer> result = new ArrayList<>();
