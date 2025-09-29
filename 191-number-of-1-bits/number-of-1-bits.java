9ed1f4db0121360926fa0152f4145663ed333109
Mahammad Aftab Umachagi <mdaftabeditz360@gmail.com>
2025-09-29 23:37:03 +0530

    Time: 0 ms (100.00%) | Memory: 40.5 MB (87.72%) - LeetSync

---
+++ 191-number-of-1-bits/number-of-1-bits.java
@@ -0,0 +1,39 @@
+public class Solution {
+    /**
+     * LeetCode Problem 190: Number of 1 Bits
+     *
+     * Write a function that takes an unsigned integer and returns the number of '1' bits it has (also known as the Hamming weight).
+     *
+     * For example, the Hamming weight of n = 11 (which is 00000000000000000000000000001011 in binary) is 3.
+     *
+     * @param n The unsigned integer
+     * @return The number of 1 bits in the binary representation of n
+     */
+    public int hammingWeight(int n) {
+        int count = 0;
+        while (n != 0) {
+            n &= (n - 1); // Clear the least significant bit (rightmost 1)
+            count++;
+        }
+        return count;
+    }
+
+    //Alternative solution using bitwise right shift operator.
+    /*
+    public int hammingWeight(int n) {
+        int count = 0;
+        for (int i = 0; i < 32; i++) {
+            count += (n & 1); // Check if the last bit is 1
+            n >>>= 1; // Unsigned right shift to move to the next bit. >>> is important to prevent sign extension for negative numbers.
+        }
+        return count;
+    }
+     */
+
+    // Another alternative solution using Integer.bitCount()
+    /*
+    public int hammingWeight(int n) {
+      return Integer.bitCount(n);
+    }
+    */
+}
