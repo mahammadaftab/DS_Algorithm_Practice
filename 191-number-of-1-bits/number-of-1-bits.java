0c9f9dd699e8b19c82b65c2346049412f3462571
Mahammad Aftab Umachagi <mdaftabeditz360@gmail.com>
2025-09-29 23:40:41 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (73.90%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.java
+++ 191-number-of-1-bits/number-of-1-bits.java
@@ -2,7 +2,6 @@ public class Solution {
     /**
      * LeetCode Problem 190: Number of 1 Bits
      *
-     * Write a function that takes an unsigned integer and returns the number of '1' bits it has (also known as the Hamming weight).
      */
     public int hammingWeight(int n) {
         int count = 0;
