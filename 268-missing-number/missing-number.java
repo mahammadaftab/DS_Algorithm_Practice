8340d5a4c4235433f5a1a2dc9e1151245c535f04
Mahammad Aftab Umachagi <mdaftabeditz360@gmail.com>
2025-09-30 19:53:25 +0530

    Time: 0 ms (100.00%) | Memory: 45.2 MB (72.70%) - LeetSync

---
+++ 268-missing-number/missing-number.java
@@ -0,0 +1,77 @@
+import java.util.Arrays;
+
+class Solution {
+    /**
+     * Given an array nums containing n distinct numbers in the range [0, n],
+     * return the only number in the range that is missing from the array.
+     *
+     * Example 1:
+     * Input: nums = [3,0,1]
+     * Output: 2
+     * Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
+     *
+     * Example 2:
+     * Input: nums = [0,1]
+     * Output: 2
+     * Explanation: n = 2 since there are 2 numbers, so all numbers are in the range [0,2]. 2 is the missing number in the range since it does not appear in nums.
+     *
+     * Example 3:
+     * Input: nums = [9,6,4,2,3,5,7,0,1]
+     * Output: 8
+     * Explanation: n = 9 since there are 9 numbers, so all numbers are in the range [0,9]. 8 is the missing number in the range since it does not appear in nums.
+     *
+     * Constraints:
+     * n == nums.length
+     * 1 <= n <= 104
+     * 0 <= nums[i] <= n
+     * All the numbers of nums are unique.
+     *
+     * @param nums The input array of distinct numbers in the range [0, n].
+     * @return The missing number in the range [0, n].
+     */
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int expectedSum = n * (n + 1) / 2;
+        int actualSum = 0;
+
+        for (int num : nums) {
+            actualSum += num;
+        }
+
+        return expectedSum - actualSum;
+    }
+
+
+    /**
+     * Another solution using bit manipulation (XOR).
+     * The idea is to XOR all numbers from 0 to n with the numbers in the array.
+     * The remaining number after XORing everything is the missing number.
+     *
+     * @param nums The input array of distinct numbers in the range [0, n].
+     * @return The missing number in the range [0, n].
+     */
+    public int missingNumberBitManipulation(int[] nums) {
+        int missing = nums.length;
+        for (int i = 0; i < nums.length; i++) {
+            missing ^= i ^ nums[i];
+        }
+        return missing;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        // Test cases
+        int[] nums1 = {3, 0, 1};
+        System.out.println("Missing number in " + Arrays.toString(nums1) + ": " + solution.missingNumber(nums1));
+        System.out.println("Missing number in " + Arrays.toString(nums1) + " (Bit Manipulation): " + solution.missingNumberBitManipulation(nums1));
+
+        int[] nums2 = {0, 1};
+        System.out.println("Missing number in " + Arrays.toString(nums2) + ": " + solution.missingNumber(nums2));
+        System.out.println("Missing number in " + Arrays.toString(nums2) + " (Bit Manipulation): " + solution.missingNumberBitManipulation(nums2));
+
+        int[] nums3 = {9, 6, 4, 2, 3, 5, 7, 0, 1};
+        System.out.println("Missing number in " + Arrays.toString(nums3) + ": " + solution.missingNumber(nums3));
+        System.out.println("Missing number in " + Arrays.toString(nums3) + " (Bit Manipulation): " + solution.missingNumberBitManipulation(nums3));
+    }
+}
