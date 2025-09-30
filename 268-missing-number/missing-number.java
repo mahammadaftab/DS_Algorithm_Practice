a42e36c5e88005e4653941f5b013f7c8f345071e
Mahammad Aftab Umachagi <mdaftabeditz360@gmail.com>
2025-09-30 19:58:58 +0530

    Time: 0 ms (100.00%) | Memory: 45.5 MB (32.48%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -7,11 +7,6 @@ class Solution {
      *
      * Example 1:
      * Input: nums = [3,0,1]
-     * Output: 2
-     * Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
-     *
-     * Example 2:
-     * Input: nums = [0,1]
      */
     public int missingNumber(int[] nums) {
         int n = nums.length;
