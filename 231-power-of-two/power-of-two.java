2d66c24fb6e8293ef2f0173edf1e0a422b2b8e2c
Mahammad Aftab Umachagi <mdaftabeditz360@gmail.com>
2025-10-01 23:35:28 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (67.99%) - LeetSync

--- 231-power-of-two/power-of-two.java
+++ 231-power-of-two/power-of-two.java
@@ -2,9 +2,6 @@ class Solution {
     /**
      * Given an integer n, return true if it is a power of two. Otherwise, return false.
      *
-     * An integer n is a power of two, if there exists an integer x such that n == 2x.
-     *
-     * Example 1:
      */
     public boolean isPowerOfTwo(int n) {
         // Negative numbers and zero are not powers of two
