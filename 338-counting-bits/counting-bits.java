3b4b33358411b15bddd31c3e08587dc901c0126c
Mahammad Aftab Umachagi <mdaftabeditz360@gmail.com>
2025-10-02 17:58:42 +0530

    Time: 2 ms (94.09%) | Memory: 45.2 MB (99.99%) - LeetSync

--- 338-counting-bits/counting-bits.java
+++ 338-counting-bits/counting-bits.java
@@ -5,10 +5,6 @@ public class Solution {
     /**
      * LeetCode Problem 338: Counting Bits
      * Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), ans[i] is the number of 1's in the binary representation of i.
-     *
-     * Example 1:
-     * Input: n = 2
-     * Output: [0,1,1]
      */
     public int[] countBits(int n) {
         int[] ans = new int[n + 1];
