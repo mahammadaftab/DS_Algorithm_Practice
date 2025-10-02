9de69f90159c0bc4b476e097c2b5e30c14d77dd5
Mahammad Aftab Umachagi <mdaftabeditz360@gmail.com>
2025-10-02 17:58:03 +0530

    Time: 2 ms (94.09%) | Memory: 46.4 MB (94.18%) - LeetSync

--- 338-counting-bits/counting-bits.java
+++ 338-counting-bits/counting-bits.java
@@ -17,13 +17,6 @@ public class Solution {
      * Example 2:
      * Input: n = 5
      * Output: [0,1,1,2,1,2]
-     * Explanation:
-     * 0 --> 0
-     * 1 --> 1
-     * 2 --> 10
-     * 3 --> 11
-     * 4 --> 100
-     * 5 --> 101
      */
     public int[] countBits(int n) {
         int[] ans = new int[n + 1];
