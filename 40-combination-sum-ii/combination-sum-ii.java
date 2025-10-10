b4dc7cd697d7d7605df629ba6b486b94045a1fbc
Mahammad Aftab Umachagi <mdaftabeditz360@gmail.com>
2025-10-10 22:47:46 +0530

    Time: 4 ms (38.73%) | Memory: 43.6 MB (23.45%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.java
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -4,8 +4,6 @@ import java.util.List;
 
 public class Solution {
 
-    /**
-     */
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         List<List<Integer>> result = new ArrayList<>();
         Arrays.sort(candidates); // Sort the candidates to easily skip duplicates.
