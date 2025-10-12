239822f7c1844f0afa01fea9225f3858dad38e42
Mahammad Aftab Umachagi <mdaftabeditz360@gmail.com>
2025-10-12 18:54:26 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (18.82%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -3,8 +3,6 @@ import java.util.List;
 
 public class Solution {
 
-    /**
-     */
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
         int n = nums.length;
