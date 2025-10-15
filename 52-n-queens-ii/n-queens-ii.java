5407d390c19cef113c12fc8ed846dd11a2d722f0
Mahammad Aftab Umachagi <mdaftabeditz360@gmail.com>
2025-10-15 22:27:45 +0530

    Time: 4 ms (17.16%) | Memory: 41.5 MB (13.37%) - LeetSync

--- 52-n-queens-ii/n-queens-ii.java
+++ 52-n-queens-ii/n-queens-ii.java
@@ -13,9 +13,7 @@ public class Solution {
      * Given an integer n, return the number of distinct solutions to the n-queens puzzle.
      *
      * Approach:
-     * Use backtracking to explore all possible placements of queens. Keep track of the columns, diagonals, and anti-diagonals that are under attack.
-     *
-''     */
+     */
 
     private int count;
 
