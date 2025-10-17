865940e5b1504aa2e414be50e9f1a47cb92390e1
Mahammad Aftab Umachagi <mdaftabeditz360@gmail.com>
2025-10-17 23:38:49 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (43.88%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.java
+++ 216-combination-sum-iii/combination-sum-iii.java
@@ -8,8 +8,6 @@ public class Solution {
      * Find all valid combinations of k numbers that sum up to n such that:
      * - Only numbers 1 through 9 are used.
      * - Each number is used at most once.
-     * - Return a list of all possible valid combinations. The list should not contain the same combination twice, and the combinations may be returned in any order.
-     *
      */
     public List<List<Integer>> combinationSum3(int k, int n) {
         List<List<Integer>> result = new ArrayList<>();
