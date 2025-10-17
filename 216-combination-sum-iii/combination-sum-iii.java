6293bfa49feba58153d798602c1c27ed3dda9897
Mahammad Aftab Umachagi <mdaftabeditz360@gmail.com>
2025-10-17 23:39:13 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (79.57%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.java
+++ 216-combination-sum-iii/combination-sum-iii.java
@@ -6,7 +6,6 @@ public class Solution {
     /**
      * LeetCode Problem 216: Combination Sum III
      * Find all valid combinations of k numbers that sum up to n such that:
-     * - Only numbers 1 through 9 are used.
      */
     public List<List<Integer>> combinationSum3(int k, int n) {
         List<List<Integer>> result = new ArrayList<>();
