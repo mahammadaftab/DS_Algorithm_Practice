fcb630fe61d56fcbefefe309848fd92fb265ba09
Mahammad Aftab Umachagi <mdaftabeditz360@gmail.com>
2025-11-02 22:33:12 +0530

    Time: 2 ms (97.63%) | Memory: 43.1 MB (5.36%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,6 +1,18 @@
 import java.util.Stack;
 
 class Solution {
+    /**
+     * Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+     *
+     * An input string is valid if:
+     *
+     * Open brackets must be closed by the same type of brackets.
+     * Open brackets must be closed in the correct order.
+     * Every close bracket has a corresponding open bracket of the same type.
+     *
+     * @param s The input string containing parentheses.
+     * @return true if the string is valid, false otherwise.
+     */
     public boolean isValid(String s) {
         // Use a stack to keep track of opening parentheses.
         Stack<Character> stack = new Stack<>();
