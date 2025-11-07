666552050ac09c30395905d9bd790aa630858f0d
Mahammad Aftab Umachagi <mdaftabeditz360@gmail.com>
2025-11-07 23:21:03 +0530

    Time: 1 ms (29.26%) | Memory: 42.8 MB (7.96%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.java
+++ 678-valid-parenthesis-string/valid-parenthesis-string.java
@@ -8,7 +8,7 @@ class Solution {
      * Any left parenthesis '(' must have a corresponding right parenthesis ')'.
      * Any right parenthesis ')' must have a corresponding left parenthesis '('.
      * Left parenthesis '(' must go before the corresponding right parenthesis ')'.
-     * '*' could be treated as a single right parenthesis ')' or a single left parenthesis '('      *
+        *
      * Example 1:
      * Input: s = "()"
      * Output: true
