b8ec0cc0699a4225f69e6c68a16275f4d09fb93e
Mahammad Aftab Umachagi <mdaftabeditz360@gmail.com>
2025-11-28 21:58:38 +0530

    Time: 6 ms (59.63%) | Memory: 47.5 MB (37.58%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
@@ -40,8 +40,7 @@ class Solution {
         return root;
     }
 
-    // Definition for a binary tree node.
-    
+    // Definition for a binary tree node.    
 
     //Example usage (not part of the leetcode submission)
     public static void main(String[] args) {
