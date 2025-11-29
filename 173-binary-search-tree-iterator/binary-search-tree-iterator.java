6dbb87e968ec163e090b4b569e232ea5a434bbc6
Mahammad Aftab Umachagi <mdaftabeditz360@gmail.com>
2025-11-29 20:07:24 +0530

    Time: 17 ms (75.01%) | Memory: 50.1 MB (21.57%) - LeetSync

--- 173-binary-search-tree-iterator/binary-search-tree-iterator.java
+++ 173-binary-search-tree-iterator/binary-search-tree-iterator.java
@@ -57,5 +57,4 @@ class BSTIterator {
  * Your BSTIterator object will be instantiated and called as such:
  * BSTIterator obj = new BSTIterator(root);
  * int param_1 = obj.next();
- * boolean param_2 = obj.hasNext();
  */
