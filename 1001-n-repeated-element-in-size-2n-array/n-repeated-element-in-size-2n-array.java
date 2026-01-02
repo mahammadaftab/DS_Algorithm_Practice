46abb6dcb88332e8d087891a9bed662583e3231d
Mahammad Aftab Umachagi <mdaftabeditz360@gmail.com>
2026-01-02 21:53:35 +0530

    Time: 0 ms (100.00%) | Memory: 47.8 MB (12.74%) - LeetSync

---
+++ 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int repeatedNTimes(int[] nums) {
+        // We check every element against its next 2 neighbors.
+        // The repeated element is guaranteed to be found within distance 2.
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (nums[i] == nums[i + 1] || nums[i] == nums[i + 2]) {
+                return nums[i];
+            }
+        }
+        
+        // If we haven't found it by the end of the loop, 
+        // the repeated element must be the very last one.
+        // Consider the case [2, 1, 2, 5] -> loop checks index 0, 1. 
+        // Returns 2.
+        // Consider edge case [1, 2, 3, 1] -> loop checks index 0, 1. 
+        // 1==2? No. 1==3? No. Returns nums[3] which is 1.
+        return nums[nums.length - 1];
+    }
+}
