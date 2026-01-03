085e115a0a6595bcf5609f83045e0e9f8270796f
Mahammad Aftab Umachagi <mdaftabeditz360@gmail.com>
2026-01-03 10:26:17 +0530

    Time: 3 ms (93.85%) | Memory: 42.1 MB (50.26%) - LeetSync

---
+++ 1527-number-of-ways-to-paint-n-3-grid/number-of-ways-to-paint-n-3-grid.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int numOfWays(int n) {
+        long mod = 1_000_000_007;
+        
+        // Base case for n = 1
+        long abc = 6;
+        long aba = 6;
+        
+        for (int i = 1; i < n; i++) {
+            // Calculate next counts based on recurrence relation
+            long nextAbc = (2 * abc + 2 * aba) % mod;
+            long nextAba = (2 * abc + 3 * aba) % mod;
+            
+            // Update states
+            abc = nextAbc;
+            aba = nextAba;
+        }
+        
+        return (int) ((abc + aba) % mod);
+    }
+}
