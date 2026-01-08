f11bd78a5fa4deda105269bdf4fdfc1465180816
Mahammad Aftab Umachagi <mdaftabeditz360@gmail.com>
2026-01-08 07:07:06 +0530

    Time: 14 ms (27.41%) | Memory: 46.5 MB (42.96%) - LeetSync

---
+++ 1569-max-dot-product-of-two-subsequences/max-dot-product-of-two-subsequences.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int maxDotProduct(int[] nums1, int[] nums2) {
+        int n = nums1.length;
+        int m = nums2.length;
+        
+        int[][] dp = new int[n][m];
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                int product = nums1[i] * nums2[j];
+                
+                // Base choice: Start a new subsequence with just this pair
+                dp[i][j] = product;
+                
+                // Choice 1: Extend the diagonal (if indices allow)
+                if (i > 0 && j > 0) {
+                    // We take max of just the product (restart) 
+                    // vs product + prev (extend)
+                    dp[i][j] = Math.max(dp[i][j], product + dp[i-1][j-1]);
+                }
+                
+                // Choice 2: Inherit from skipping nums1[i]
+                if (i > 0) {
+                    dp[i][j] = Math.max(dp[i][j], dp[i-1][j]);
+                }
+                
+                // Choice 3: Inherit from skipping nums2[j]
+                if (j > 0) {
+                    dp[i][j] = Math.max(dp[i][j], dp[i][j-1]);
+                }
+            }
+        }
+        
+        return dp[n-1][m-1];
+    }
+}
