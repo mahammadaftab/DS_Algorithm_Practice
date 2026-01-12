fcb14ba7ae175fdbb19ff19c4005dcda6168ff59
Mahammad Aftab Umachagi <mdaftabeditz360@gmail.com>
2026-01-12 19:06:29 +0530

    Time: 1 ms (95.28%) | Memory: 44.8 MB (48.65%) - LeetSync

---
+++ 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minTimeToVisitAllPoints(int[][] points) {
+        int totalTime = 0;
+        
+        // Iterate through all pairs of consecutive points
+        for (int i = 0; i < points.length - 1; i++) {
+            int x1 = points[i][0], y1 = points[i][1];
+            int x2 = points[i + 1][0], y2 = points[i + 1][1];
+            
+            // The time between two points is the max of the X and Y differences
+            int deltaX = Math.abs(x2 - x1);
+            int deltaY = Math.abs(y2 - y1);
+            
+            totalTime += Math.max(deltaX, deltaY);
+        }
+        
+        return totalTime;
+    }
+}
