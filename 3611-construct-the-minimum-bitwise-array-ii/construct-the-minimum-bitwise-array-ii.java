75a37abf151dadd03afe0b68bb6033321a52c625
Mahammad Aftab Umachagi <mdaftabeditz360@gmail.com>
2026-01-21 08:24:26 +0530

    Time: 1 ms (100.00%) | Memory: 46.9 MB (55.56%) - LeetSync

---
+++ 3611-construct-the-minimum-bitwise-array-ii/construct-the-minimum-bitwise-array-ii.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public int[] minBitwiseArray(List<Integer> nums) {
+        int n = nums.size();
+        int[] ans = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            int val = nums.get(i);
+            
+            if (val == 2) { 
+                ans[i] = -1;
+            } else if (val % 2 == 0) {
+                // Any even number cannot be formed by x | (x+1)
+                ans[i] = -1;
+            } else {
+                // Use long to prevent overflow during 'val + 1' if val is Integer.MAX_VALUE
+                long nextVal = (long) val + 1;
+                
+                // Get the lowest set bit of (val + 1)
+                long lowBit = nextVal & -nextVal;
+                
+                // The bit we want to remove from 'val' is lowBit shifted right by 1
+                ans[i] = val - (int)(lowBit >> 1);
+            }
+        }
+        
+        return ans;
+    }
+}
