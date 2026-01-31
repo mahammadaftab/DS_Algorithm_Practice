695011dd1330771f84125c3e8cd8cdf51bfa0cfc
Mahammad Aftab Umachagi <mdaftabeditz360@gmail.com>
2026-01-31 19:09:26 +0530

    Time: 0 ms (100.00%) | Memory: 46.3 MB (65.72%) - LeetSync

---
+++ 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.java
@@ -0,0 +1,20 @@
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        int low = 0;
+        int high = letters.length;
+
+        // Standard Binary Search for Upper Bound
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            
+            if (letters[mid] <= target) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+
+        // If low == letters.length, it wraps around to 0
+        return letters[low % letters.length];
+    }
+}
