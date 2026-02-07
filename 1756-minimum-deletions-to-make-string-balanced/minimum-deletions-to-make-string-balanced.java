74c1ecda83ee9eb3fa9848e65aba863444495d21
Mahammad Aftab Umachagi <mdaftabeditz360@gmail.com>
2026-02-07 18:54:04 +0530

    Time: 19 ms (93.09%) | Memory: 47.9 MB (30.43%) - LeetSync

---
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int minimumDeletions(String s) {
+        int countB = 0;
+        int minDeletions = 0;
+        
+        for (char c : s.toCharArray()) {
+            if (c == 'b') {
+                // Just count the 'b', it doesn't cause a conflict yet
+                countB++;
+            } else {
+                // c == 'a'
+                // Conflict arises! We have two options:
+                // 1. Delete this 'a' (cost: current minDeletions + 1)
+                // 2. Keep this 'a' (implies deleting all previous 'b's: cost countB)
+                minDeletions = Math.min(minDeletions + 1, countB);
+            }
+        }
+        
+        return minDeletions;
+    }
+}
