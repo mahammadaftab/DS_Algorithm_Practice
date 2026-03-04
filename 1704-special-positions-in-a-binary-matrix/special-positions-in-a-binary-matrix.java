9f3385c66c49f6ac6a77c436df23d689e441bc6e
Mahammad Aftab Umachagi <mdaftabeditz360@gmail.com>
2026-03-04 06:27:02 +0530

    Time: 2 ms (94.84%) | Memory: 47.3 MB (44.84%) - LeetSync

---
+++ 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int numSpecial(int[][] mat) {
+        int m = mat.length;
+        int n = mat[0].length;
+        
+        int[] rowCount = new int[m];
+        int[] colCount = new int[n];
+        
+        // 1. Precompute the number of 1s in each row and column
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == 1) {
+                    rowCount[i]++;
+                    colCount[j]++;
+                }
+            }
+        }
+        
+        int specialCount = 0;
+        
+        // 2. Check each 1 to see if it's the only one in its row and col
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == 1 && rowCount[i] == 1 && colCount[j] == 1) {
+                    specialCount++;
+                }
+            }
+        }
+        
+        return specialCount;
+    }
+}
+
