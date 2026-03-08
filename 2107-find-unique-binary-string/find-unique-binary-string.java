3cd2c949ce878c21e3ba61b49d2b66e941eb1521
Mahammad Aftab Umachagi <mdaftabeditz360@gmail.com>
2026-03-08 06:37:24 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (64.70%) - LeetSync

---
+++ 2107-find-unique-binary-string/find-unique-binary-string.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String findDifferentBinaryString(String[] nums) {
+        StringBuilder ans = new StringBuilder();
+        
+        for (int i = 0; i < nums.length; i++) {
+            // Get the diagonal character
+            char diagonalChar = nums[i].charAt(i);
+            
+            // Append the flipped character to our answer
+            if (diagonalChar == '0') {
+                ans.append('1');
+            } else {
+                ans.append('0');
+            }
+        }
+        
+        return ans.toString();
+    }
+}
