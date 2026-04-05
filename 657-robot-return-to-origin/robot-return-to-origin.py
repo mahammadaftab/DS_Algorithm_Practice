9b5966b5df8e3d68984a94971e42a2c0aa236f74
Mahammad Aftab Umachagi <mdaftabeditz360@gmail.com>
2026-04-05 14:07:47 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (92.29%) - LeetSync

--- 657-robot-return-to-origin/robot-return-to-origin.py
+++ 657-robot-return-to-origin/robot-return-to-origin.py
@@ -2,4 +2,3 @@ class Solution:
     def judgeCircle(self, moves: str) -> bool:
         # The robot only returns to origin if Up/Down cancel out AND Left/Right cancel out
         return moves.count('U') == moves.count('D') and moves.count('L') == moves.count('R')
-
