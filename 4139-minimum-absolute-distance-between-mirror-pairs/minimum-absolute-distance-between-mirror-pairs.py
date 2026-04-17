c3baa3b565e225f6c988334e7ec2d58473f14575
Mahammad Aftab Umachagi <mdaftabeditz360@gmail.com>
2026-04-17 07:41:13 +0530

    Time: 214 ms (75.19%) | Memory: 42.2 MB (39.53%) - LeetSync

---
+++ 4139-minimum-absolute-distance-between-mirror-pairs/minimum-absolute-distance-between-mirror-pairs.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minMirrorPairDistance(self, nums: list[int]) -> int:
+        pos = {}
+        min_dist = float('inf')
+        
+        for j, num in enumerate(nums):
+            # 1. Check if there is an earlier 'i' where reverse(nums[i]) == nums[j]
+            if num in pos:
+                min_dist = min(min_dist, j - pos[num])
+                
+            # 2. Store the reversed value of the current number for future 'j's to match against
+            # str(num)[::-1] converts to string, reverses it, then int() drops leading zeros
+            rev_num = int(str(num)[::-1])
+            pos[rev_num] = j
+            
+        return min_dist if min_dist != float('inf') else -1
