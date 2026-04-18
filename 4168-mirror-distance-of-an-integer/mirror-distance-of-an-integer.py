d6ed820af36a13a68a0830b62d3d0e161842a1dc
Mahammad Aftab Umachagi <mdaftabeditz360@gmail.com>
2026-04-18 09:03:06 +0530

    Time: 3 ms (13.40%) | Memory: 19.1 MB (98.66%) - LeetSync

--- 4168-mirror-distance-of-an-integer/mirror-distance-of-an-integer.py
+++ 4168-mirror-distance-of-an-integer/mirror-distance-of-an-integer.py
@@ -2,4 +2,3 @@ class Solution:
     def mirrorDistance(self, n: int) -> int:
         # Convert to string, reverse it, convert back to int, and calculate absolute difference
         return abs(n - int(str(n)[::-1]))
-
