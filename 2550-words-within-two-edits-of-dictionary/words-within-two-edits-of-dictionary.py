796ced7e2853ab4678f66853014080ff924710dc
Mahammad Aftab Umachagi <mdaftabeditz360@gmail.com>
2026-04-22 11:44:40 +0530

    Time: 15 ms (63.24%) | Memory: 19.5 MB (50.49%) - LeetSync

---
+++ 2550-words-within-two-edits-of-dictionary/words-within-two-edits-of-dictionary.py
@@ -0,0 +1,22 @@
+class Solution:
+    def twoEditWords(self, queries: list[str], dictionary: list[str]) -> list[str]:
+        ans = []
+        
+        for query in queries:
+            for dict_word in dictionary:
+                # Count how many characters differ at the same index
+                edits = 0
+                for c1, c2 in zip(query, dict_word):
+                    if c1 != c2:
+                        edits += 1
+                    
+                    # Optimization: Early exit if we already exceeded 2 edits
+                    if edits > 2:
+                        break
+                        
+                # If we finished the word with 2 or fewer edits, it's a match
+                if edits <= 2:
+                    ans.append(query)
+                    break # Stop checking this query against the rest of the dictionary
+                    
+        return ans
