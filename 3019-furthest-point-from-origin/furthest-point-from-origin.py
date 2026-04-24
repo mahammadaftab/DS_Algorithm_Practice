4003b6fef5798504053e9f4cfb07a8010a38b75d
Mahammad Aftab Umachagi <mdaftabeditz360@gmail.com>
2026-04-24 07:15:15 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (18.15%) - LeetSync

--- 3019-furthest-point-from-origin/furthest-point-from-origin.py
+++ 3019-furthest-point-from-origin/furthest-point-from-origin.py
@@ -3,3 +3,4 @@ class Solution:
         # The absolute difference between L and R gives our net deterministic distance.
         # We then add all wildcards (_) to push us further in that winning direction.
         return abs(moves.count('L') - moves.count('R')) + moves.count('_')
+
