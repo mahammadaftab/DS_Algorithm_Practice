f8a8e7eebc9a96ada3ae409b2312ed086e04f087
Mahammad Aftab Umachagi <mdaftabeditz360@gmail.com>
2026-05-25 09:09:41 +0530

    Time: 130 ms (81.01%) | Memory: 20.6 MB (90.38%) - LeetSync

---
+++ 2001-jump-game-vii/jump-game-vii.py
@@ -0,0 +1,28 @@
+class Solution:
+    def canReach(self, s: str, minJump: int, maxJump: int) -> bool:
+        n = len(s)
+        # If the last character is a wall, it's impossible from the start
+        if s[-1] == '1':
+            return False
+            
+        dp = [False] * n
+        dp[0] = True  # Base case: we start at index 0
+        
+        reachable_count = 0
+        
+        for i in range(1, n):
+            # 1. Add new available jumping source to the right side of the window
+            if i >= minJump:
+                if dp[i - minJump]:
+                    reachable_count += 1
+                    
+            # 2. Remove the outdated jumping source from the left side of the window
+            if i > maxJump:
+                if dp[i - maxJump - 1]:
+                    reachable_count -= 1
+                    
+            # 3. If the window contains at least one valid source and current spot is '0'
+            if s[i] == '0' and reachable_count > 0:
+                dp[i] = True
+                
+        return dp[n - 1]
