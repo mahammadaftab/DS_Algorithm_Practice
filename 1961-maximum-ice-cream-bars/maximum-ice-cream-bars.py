f7df6ab6bcacc4cc0b91908a5f9ef294520aee6d
Mahammad Aftab Umachagi <mdaftabeditz360@gmail.com>
2026-06-21 11:15:30 +0530

    Time: 86 ms (45.12%) | Memory: 32 MB (44.03%) - LeetSync

---
+++ 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxIceCream(self, costs: list[int], coins: int) -> int:
+        # 1. Sort the costs to prioritize the cheapest ice cream bars
+        costs.sort()
+        count = 0
+        
+        # 2. Buy sequentially until we run out of coins
+        for cost in costs:
+            if coins >= cost:
+                coins -= cost
+                count += 1
+            else:
+                # Since the array is sorted, we cannot afford any remaining bars
+                break
+                
+        return count
